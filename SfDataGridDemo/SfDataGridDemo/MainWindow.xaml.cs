2b255af092159569118f59b5debf407b3a8421b1
vijayarasan <58214164+vijayarasan@users.noreply.github.com>
2021-11-25 13:06:19 +0530

    Sample Added
    Sample Added

---
+++ SfDataGridDemo/SfDataGridDemo/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Syncfusion.UI.Xaml.DataGrid;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace SfDataGridDemo
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            this.dataGrid.CurrentCellValueChanged += OnCurrentCellValueChanged;
+        }
+
+        private void OnCurrentCellValueChanged(object sender, CurrentCellValueChangedEventArgs e)
+        {
+            var updateValue = this.dataGrid.GetUnboundCellValue(dataGrid.Columns[6], this.dataGrid.CurrentItem);
+        }       
+    }   
+}
